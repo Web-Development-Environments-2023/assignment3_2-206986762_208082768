5b889aa7d0448652aada4eaac7c39189abaa327d
eliya <eliyaballout@gmail.com>
2023-06-02 19:15:35 +0300

    recipes route almost done!

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,19 +2,66 @@ var express = require("express");
 var router = express.Router();
 const recipes_utils = require("./utils/recipes_utils");
 
-router.get("/", (req, res) => res.send("im here"));
+//router.get("/", (req, res) => res.send("im here"));
+
 
 
 /**
- * This path returns a full details of a recipe by its id
+ * This path returns a full details of a recipe by its id.
  */
-router.get("/:recipeId", async (req, res, next) => {
+router.get("/:recipeId", async (req, res, next) => { //done and checked
   try {
     const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
     res.send(recipe);
-  } catch (error) {
+  } 
+  catch (error) {
     next(error);
   }
 });
+
+
+/**
+ * This path returns the logged in user search of a specific recipes according to his choices.
+ */
+router.post("/search", async (req, res, next) => { //done and checked
+  try {
+    const recipes = await recipes_utils.search(req.query.query, req.query.number, req.query.cuisine, req.query.diet, req.query.intolerance, req.query.sort);
+    res.send(recipes);
+  } 
+  catch (error) {
+    next(error);
+  }
+});
+
+
+/**
+ * This path returns 3 random recipes.
+ */
+router.post("/randomRecipes", async (req, res, next) => { //done and checked
+  try {
+    const randomRecipes = await recipes_utils.getRandomRecipes();
+    res.send(randomRecipes);
+  } 
+  catch (error) {
+    next(error);
+  }
+});
+
+
+/**
+ * This path add new recipe to the database.
+ */
+router.post("/createRecipe", async (req, res, next) => {
+  try {
+    const newRecipes = await recipes_utils.addNewRecipe();
+    res.send(newRecipes);
+  } 
+  catch (error) {
+    next(error);
+  }
+});
+
+
+
 
 module.exports = router;
--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const DButils = require("./DButils");
 const api_domain = "https://api.spoonacular.com/recipes";
 
 
@@ -7,8 +8,6 @@ const api_domain = "https://api.spoonacular.com/recipes";
  * Get recipes list from spooncular response and extract the relevant recipe data for preview
  * @param {*} recipes_info 
  */
-
-
 async function getRecipeInformation(recipe_id) {
     return await axios.get(`${api_domain}/${recipe_id}/information`, {
         params: {
@@ -17,7 +16,6 @@ async function getRecipeInformation(recipe_id) {
         }
     });
 }
-
 
 
 async function getRecipeDetails(recipe_id) {
@@ -48,6 +46,61 @@ async function getRecipesPreview( recipes_id_array ){
 }
 
 
+async function search(query, number = 5, cuisine, diet, intolerance, sort){
+    //preparing the serach URL
+    let url = `${api_domain}/complexSearch/?query=${query}&number=${number}&instructionsRequired=true&addRecipeInformation=true`
+    if(cuisine !== undefined){
+        url = url + `&cuisine=${cuisine}`
+    }
+
+    if(diet !== undefined){
+        url = url + `&diet=${diet}`
+    }
+
+    if(intolerance !== undefined){
+        url = url + `&intolerance=${intolerance}`
+    }
+
+    if(sort !== undefined){
+        url = url + `&sort=${sort}`
+    }
+
+    //extracting the recipes
+    const response = await axios.get(url,{
+        
+        params: {
+            apiKey: process.env.spooncular_apiKey
+        }
+    });
+
+    const searchRecipes = response.data["results"]
+    let recipes = []
+    for (let i = 0; i < searchRecipes.length; i++){
+        recipes.push(await getRecipeDetails(searchRecipes[i].id))
+    }
+
+    return recipes.slice(0,number)
+}
+
+
+async function getRandomRecipes(){
+    const response = await axios.get(`${api_domain}/random?number=3&apiKey=${process.env.spooncular_apiKey}`)
+
+    const searchRecipes = response.data["recipes"]
+    let recipes = []
+    for (let i = 0; i < searchRecipes.length; i++){
+        recipes.push(await getRecipeDetails(searchRecipes[i].id))
+    }
+
+    return recipes.slice(0,3)
+}
+
+
+async function addNewRecipe(){
+    //TODO complete this function
+}
+
+
 
 // #####################################################################################################
 // ##################################### Export all functions ##########################################
@@ -55,6 +108,6 @@ async function getRecipesPreview( recipes_id_array ){
 
 exports.getRecipeDetails = getRecipeDetails;
 exports.getRecipesPreview = getRecipesPreview;
-
-
-
+exports.search = search;
+exports.getRandomRecipes = getRandomRecipes;
+exports.addNewRecipe = addNewRecipe;
